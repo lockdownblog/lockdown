6579ce4c6772b19e848b0cf838ca52fa9d90dd69
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-11-15 08:25:51 +0000

    Add build command

---
+++ Lockdown/Build/LockdownFileSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using DotLiquid;
+using DotLiquid.Exceptions;
+using DotLiquid.FileSystems;
+
+namespace Lockdown.Build
+{
+    public class LockdownFileSystem : IFileSystem
+    {
+        public string Root { get; set; }
+
+        public LockdownFileSystem(string root)
+        {
+            Root = Path.GetFullPath(root);
+        }
+
+        public string ReadTemplateFile(Context context, string templateName)
+        {
+            string templatePath = (string)context[templateName];
+            string fullPath = FullPath(templatePath);
+            if (!File.Exists(fullPath))
+                throw new FileSystemException("The template file {0} does not exist", templatePath);
+            return File.ReadAllText(fullPath);
+        }
+
+        public string FullPath(string templatePath)
+        {
+            if (templatePath == null || !Regex.IsMatch(templatePath, @"^[^.\/][a-zA-Z0-9_\/]+$"))
+                throw new FileSystemException("Illegal template path name {0}", templatePath);
+
+            string fullPath = templatePath.Contains("/")
+                ? Path.Combine(Path.Combine(Root, Path.GetDirectoryName(templatePath)), string.Format("_{0}.liquid", Path.GetFileName(templatePath)))
+                : Path.Combine(Root, string.Format("_{0}.liquid", templatePath));
+
+            string escapedPath = Regex.Escape(Root);
+            
+            if (!fullPath.StartsWith(escapedPath))
+            {
+                throw new FileSystemException("The path {0} does not exist in the specified root {1}",fullPath, escapedPath);
+            }
+
+            return fullPath;
+        }
+    }
+}
---
+++ example/content/posts/hello-world.md
@@ -0,0 +1,20 @@
+---
+layout: post
+title: "What is lockdown?"
+tags: dotnet
+image: /assets/images/posts/misc/parse-frontmatter.jpg
+image_credit_name: Ben Sweet
+image_credit_url: https://unsplash.com/@benjaminsweet
+image_alt: Man standing in dark room blue light
+---
+
+## Hello world!
+
+*Lockdown* is a static website generator completely written in C#  
+
+It uses the following libraries:  
+
+ - DotLiquid [GitHub](https://github.com/dotliquid/dotliquid)
+ - Markdig [GitHub](https://github.com/lunet-io/markdig)  
+ - YamlDotNet [GitHub](https://github.com/aaubry/YamlDotNet)  
+ - CommandLineUtils [GitHub](https://github.com/natemcmaster/CommandLineUtils)

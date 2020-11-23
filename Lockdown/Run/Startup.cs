38f9a62ae37264579dccbfc38e3b8bea8cd72fc2
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-11-23 07:05:07 +0000

    update startup

--- Lockdown/Run/Startup.cs
+++ Lockdown/Run/Startup.cs
@@ -1,6 +1,5 @@
 namespace Lockdown.Run
 {
-    using System.IO;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.FileProviders;

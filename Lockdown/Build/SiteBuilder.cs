b542d7d5b5674af9279737a5e569dd0148101e76
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-11-23 20:27:30 +0000

    Add sitebuilder

--- Lockdown/Build/SiteBuilder.cs
+++ Lockdown/Build/SiteBuilder.cs
@@ -128,18 +128,54 @@
             };
         }
 
+        public IEnumerable<string> GetFilesIncludingSubfolders(string path)
+        {
+            var paths = new List<string>();
+            var directories = Directory.GetDirectories(path);
+
+            foreach (var directory in directories)
+            {
+                paths.AddRange(this.GetFilesIncludingSubfolders(directory));
+            }
+            paths.AddRange(Directory.GetFiles(path).ToList());
+            return paths;
+        }
+
         public int Build()
         {
             this.CleanOutput();
             this.siteConfig = this.GetConfig();
 
-            var postDirectory = Directory.GetFiles(this.PostsInputPath);
+            var postDirectory = this.GetFilesIncludingSubfolders(this.PostsInputPath);
 
             foreach (var file_path in postDirectory)
             {
                 var file_text = File.ReadAllText(file_path);
                 var file_name = Path.GetFileNameWithoutExtension(file_path);
-                var outFileName = $"{file_name}.html";
+                var fullFileName = file_path.Substring(this.PostsInputPath.Length).TrimStart('/', ' ');
+                var parts = fullFileName.Split('/', StringSplitOptions.RemoveEmptyEntries).SkipLast(1);
+                string newFileDirectory = Path.Combine(parts.ToArray());
+                string fileToWriteTo = null;
+                var outFileName = string.IsNullOrWhiteSpace(newFileDirectory) ? $"{file_name}.html" : Path.Combine(newFileDirectory, $"{file_name}.html");
+                string url = null;
+
+                if (parts.Count() > 0)
+                {
+                    var path = Path.Combine(this.OutputPath, newFileDirectory);
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    fileToWriteTo = Path.Combine(this.OutputPath, outFileName);
+                    url = outFileName;
+                }
+                else
+                {
+                    fileToWriteTo = Path.Combine(this.PostsOutputPath, outFileName);
+                    url = PostsPath + "/" + outFileName;
+                }
+
                 var document = Markdown.Parse(file_text, MarkdownExtensions.Pipeline);
 
                 var frontMatter = document.GetFrontMatter<PostFrontMatter>();
@@ -159,7 +195,7 @@
                 writer.Write("{% endblock %}\n");
                 writer.Flush();
 
-                frontMatter.Url = PostsPath + "/" + outFileName;
+                frontMatter.Url = url;
                 var indexPost = this.mapper.Map<IndexPost>(frontMatter);
 
                 this.posts.Add(indexPost);
@@ -169,7 +205,7 @@
                 var template = Template.Parse(writer.ToString());
                 var rendered = template.Render(Hash.FromAnonymousObject(siteVars));
 
-                using var file = new System.IO.StreamWriter(Path.Combine(this.PostsOutputPath, outFileName));
+                using var file = new System.IO.StreamWriter(fileToWriteTo);
                 file.Write(rendered);
             }
 
